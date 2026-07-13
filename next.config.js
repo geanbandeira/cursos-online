b0840fc229a843974524c306db8ca8d4d4b1b70b
Gean <gean0606@hotmail.com>
2026-07-13 14:27:09 -0300

    Adiciona redirecionamentos

---
+++ next.config.js
@@ -0,0 +1,54 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://cursos-online.masterproject.com.br/',
+        permanent: true,
+      },
+      {
+        source: '/course/1',
+        destination: 'https://cursos-online.masterproject.com.br/okr',
+        permanent: true,
+      },
+      {
+        source: '/course/2',
+        destination: 'https://cursos-online.masterproject.com.br/jira-software',
+        permanent: true,
+      },
+      {
+        source: '/course/3',
+        destination: 'https://cursos-online.masterproject.com.br/bpmn-com-bizagi',
+        permanent: true,
+      },
+      {
+        source: '/course/4',
+        destination: 'https://cursos-online.masterproject.com.br/bpm',
+        permanent: true,
+      },
+      {
+        source: '/course/5',
+        destination: 'https://cursos-online.masterproject.com.br/pmi-da-iniciacao-ao-planejamento',
+        permanent: true,
+      },
+      {
+        source: '/course/6',
+        destination: 'https://cursos-online.masterproject.com.br/pmi-planejamento-avancado',
+        permanent: true,
+      },
+      {
+        source: '/course/7',
+        destination: 'https://cursos-online.masterproject.com.br/pmi-execucao-monitoramento-e-controle',
+        permanent: true,
+      },
+      {
+        source: '/course/11',
+        destination: 'https://cursos-online.masterproject.com.br/aulas-abertas',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
